93ce33221c1240c2d0e83acfe49495afdaf75972
Call Carl <callienicole0419@gmail.com>
2023-01-03 19:23:20 -0600

    aded arrays for license badges and links

--- Develop/index.js
+++ Develop/index.js
@@ -4,6 +4,9 @@ const inquirer = require('inquirer');
 //fs is for reading and writing files
 const fs = require('fs');
 
+//link to js file
+const generate = require('./utils/generateMarkdown.js');
+
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -73,7 +76,7 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(README, data) {}
 
 // TODO: Create a function to initialize app
 function init() {}
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,4 +1,16 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
+const licenseBadges = [
+  '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)',
+  '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
+  '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0',
+  '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)',
+]
+const licenseLinks = [
+  'http://www.apache.org/licenses/LICENSE-2.0',
+  'http://opensource.org/licenses/MIT',
+  'https://www.mozilla.org/en-US/MPL/2.0/',
+  'https://unlicense.org/',
+]
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
 
