e53625c6758a472479f3f33b1815db1da05f13e5
Call Carl <callienicole0419@gmail.com>
2023-01-03 18:42:32 -0600

    added questions

--- Develop/index.js
+++ Develop/index.js
@@ -6,7 +6,54 @@ const fs = require('fs');
 
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?', 
+    
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a project description.',
+    
+    },
+    { 
+        type: 'input',
+        name: 'installation',
+        message: 'Please provide the installation instructions for your project:',
+    
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Add any additional usage information for the user:',
+        
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'Add any additional contributors to your project:',
+        
+    },
+    {
+        type: 'input',
+        name: 'testing',
+        message: 'Please provide test insructions:',
+    
+    },
+    //{
+      //  type: 'checkbox',
+       // name: 'license',
+        //message: 'Please choose a license for your project:',
+        //choices: [  INSERT LICENSE CHOICES
+          //  '',
+            //'',
+            //''
+        //]
+    //}
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
